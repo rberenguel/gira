6843def85f194340108edfd66e46b942eea07081
Ruben Berenguel Montoro <ruben@mostlymaths.net>
2025-03-08 20:53:39 +0100

    Duh

--- src/script.js
+++ src/script.js
@@ -301,7 +301,7 @@ let playlistUrl = await get("playlistUrl");
 console.info(`Stored URL: ${playlistUrl}`);
 
 // If no video has been ever provided I add a potential placeholder one.
-if (!playlist || !playlistUrl.trim()) {
+if (!playlistUrl || !playlistUrl.trim()) {
   playlistUrl = "https://www.youtube.com/embed/vtIzMaLkCaM?si=8yYT3QU43wvBkJNI";
 }
 
